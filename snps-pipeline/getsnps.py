a3379376255d9b59927b6953681c46758a5f6315
Kevin Ying <kvying.04@gmail.com>
2026-02-07 21:20:00 -0800

    SNPs work

--- snps-pipeline/getsnps.py
+++ snps-pipeline/getsnps.py
@@ -1,9 +1,9 @@
 import parsegtf
-import subprocess
+
 import pandas as pd
 from pathlib import Path
 
-def removeheader(vcfpath: Path, outpath: Path) -> None:
+def _removeheader(vcfpath: Path, outpath: Path) -> None:
 	with open(vcfpath, 'r') as vcf, open(outpath, 'w') as cleanvcf:
 		for line in vcf:
 			if not line.startswith("##"):
@@ -12,7 +12,7 @@ def removeheader(vcfpath: Path, outpath: Path) -> None:
 	return
 	
 
-def filterisolates(cleanpath: Path, isolates: list, filteredpath: Path) -> None:
+def _filterisolates(cleanpath: Path, isolates: list, filteredpath: Path) -> None:
 	if filteredpath == None:
 		filteredpath = cleanpath
 	
@@ -20,7 +20,9 @@ def filterisolates(cleanpath: Path, isolates: list, filteredpath: Path) -> None:
 
 	cols = id + isolates
 
+	print("\t\tReading VCF into Pandas...")
 	filtereddf = pd.read_csv(cleanpath, usecols=cols, sep='\t')
+	print("\t\tWriting to CSV...")
 	filtereddf.to_csv(filteredpath, sep='\t', index=False)
 	# colindices = []
 	
@@ -39,18 +41,43 @@ def filterisolates(cleanpath: Path, isolates: list, filteredpath: Path) -> None:
 	return
 
 
-
-
 def loadvcf(vcfpath: Path, cleanpath: Path, isolates: list, filteredpath: Path) -> pd.DataFrame:
 	print("Remaking VCF...")
 	print("\tRemoving header rows...")
-	removeheader(vcfpath, cleanpath)
+	_removeheader(vcfpath, cleanpath)
 	print("\tFiltering isolates...")
-	filterisolates(cleanpath, isolates, filteredpath)
+	_filterisolates(cleanpath, isolates, filteredpath)
 
 	vcf = pd.read_csv(filteredpath, sep='\t')
 
 	return vcf
+
+
+def _snpsofinterest_col(vcfcol: pd.Series, phenotype: str) -> pd.Series:
+	vcfcol = vcfcol.str.extract(r'(\d+)/(\d+)')
+	if phenotype == 'loss':
+		vcfcol = vcfcol.fillna(1)
+	elif phenotype == 'keep':
+		vcfcol = vcfcol.fillna(0)
+	
+	vcfcol = vcfcol.astype(int).sum(axis=1)
+	
+	return vcfcol
+
+
+def snpsofinterest(vcf: pd.DataFrame, keep_strains: list, loss_strains: list) -> pd.DataFrame:
+	interestingsnps = vcf.loc[:, '#CHROM':'FORMAT']
+
+	for isolate in keep_strains:
+		interestingsnps[isolate] = _snpsofinterest_col(vcf[isolate], 'keep')
+
+	for isolate in loss_strains:
+		interestingsnps[isolate] = _snpsofinterest_col(vcf[isolate], 'loss')
+
+	interestingsnps['max_keep'] = interestingsnps[keep_strains].max(axis=1)
+	interestingsnps['min_loss'] = interestingsnps[loss_strains].min(axis=1)
+
+	return interestingsnps[interestingsnps['max_keep'] < interestingsnps['min_loss']]
 
 
 if __name__ == "__main__":
@@ -64,4 +91,8 @@ if __name__ == "__main__":
 	loss_strains = ['CX11314']
 	isolates = keep_strains + loss_strains
 
-	vcfdf = loadvcf(VCFPATH, OUTPATH, isolates, None)
+	# vcfdf = loadvcf(VCFPATH, OUTPATH, isolates, None)
+	vcfdf = pd.read_csv(OUTPATH, sep='\t')
+
+	isnps = snpsofinterest(vcfdf, keep_strains, loss_strains)
+	isnps.to_csv(OUTDIR/"interestingsnps.tsv", sep='\t', index=False)
